569be30b0c3b7eb16248a549d9d367aca68c5f10
Dustin Johnson <dustinjohnson13@gmail.com>
2012-12-26 23:35:42 -0600

    Use base.pom as parent

--- src/test/java/com/jdom/util/j2ee/MockContext.java
+++ src/test/java/com/jdom/util/j2ee/MockContext.java
@@ -13,40 +13,28 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */package com.jdom.util.j2ee;
+ */
+package com.jdom.util.j2ee;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-import org.jmock.Expectations;
-import org.jmock.Mockery;
-import org.jmock.lib.legacy.ClassImposteriser;
-
 public final class MockContext {
 
-    private final static Mockery context = new Mockery() {
-        {
-            setImposteriser(ClassImposteriser.INSTANCE);
+	private final static InitialContext instance = mock(InitialContext.class);
 
-        }
-    };
+	public static InitialContext getInstance() {
+		return instance;
+	}
 
-    private final static InitialContext instance = context.mock(InitialContext.class);
-
-    public static InitialContext getInstance() {
-        return instance;
-    }
-
-    public static void registerInJndi(final String lookup, final Object object) {
-        try {
-            context.checking(new Expectations() {
-                {
-                    allowing(instance).lookup(lookup);
-                    will(returnValue(object));
-                }
-            });
-        } catch (NamingException e) {
-            throw new IllegalStateException(e);
-        }
-    }
+	public static void registerInJndi(final String lookup, final Object object) {
+		try {
+			when(instance.lookup(lookup)).thenReturn(object);
+		} catch (NamingException e) {
+			throw new IllegalStateException(e);
+		}
+	}
 }
